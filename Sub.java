4fe613073a55025d2980aaafb4c0ca4fca230ce8
Aditya <adityasomasi@gmail.com>
2025-02-20 10:19:14 +0530

    Added subtraction feature

---
+++ Sub.java
@@ -0,0 +1,12 @@
+public class Sub {
+    public static int subtraction(int a, int b){
+        return a - b;
+    }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = subtraction(a, b);
+        System.out.println(result);
+    }
+
+}
